77d80886e999162593e13fd915c096a3092c3701
Emanuel Sosa <emanuelesausosa@gmail.com>
2022-03-23 22:28:36 -0600

    [material-design-theme] add material design, layout and hero

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatCardModule} from "@angular/material/card";
+import { LayoutComponent } from './components/layout/layout.component';
+import { HeroComponent } from './components/hero/hero.component';
+import {RouterModule} from "@angular/router";
+import {AppRoutingModule} from "./app.routing.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HeroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatCardModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,18 @@
+import {RouterModule, Routes} from "@angular/router";
+import {NgModule} from "@angular/core";
+import {LayoutComponent} from "./components/layout/layout.component";
+
+const routes: Routes = [
+  {
+    path: "",
+    component: LayoutComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {
+    useHash: true
+  })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
